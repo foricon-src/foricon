893488aa0371af5bcad2a495a5acf67ecac65734
Namdev23 <nguyennamtiger@gmail.com>
2026-04-09 19:28:48 +0700

    09/04/26 (3)

--- src/app/page.js
+++ src/app/page.js
@@ -7,14 +7,14 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
+          src="./assets/foricon-f-favicon-png"
+          alt="FOricon logo"
           width={100}
           height={20}
           priority
         />
         <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
+          <h1>Foricon</h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
             <a
