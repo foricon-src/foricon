869a2881377c00a96c11762bd2629a4c19bde54b
Namdev23 <nguyennamtiger@gmail.com>
2026-04-09 21:47:51 +0700

    09/04/26 (18)

--- src/components/get-lang.js
+++ src/components/get-lang.js
@@ -1,45 +1,35 @@
-"use client";
-import { useEffect } from "react";
+'use client';
+import { useEffect } from 'react';
 
 export default function getLang() {
-	useEffect(() => {
-		// Check if we already have a language stored in localStorage
-		const storedLang = localStorage.getItem("preferredLang");
-		
-		if (storedLang) {
-			// Apply stored language immediately
-			document.documentElement.setAttribute("lang", storedLang);
-		}
-		
-		// Check if we've already fetched IP info this session
-		const fetchedThisSession = sessionStorage.getItem("ipinfoFetched");
-		
-		if (!fetchedThisSession) {
-			fetch("https://ipinfo.io/json")
-			.then(res => res.json())
-			.then(data => {
-				const country = data.country?.toLowerCase();
-				const countryLangMap = {
-					vn: "vi",
-					fr: "fr",
-					de: "de",
-					jp: "ja",
-					us: "en",
-				};
-				const lang = countryLangMap[country] || "en";
-				
-				// Apply detected language
-				document.documentElement.setAttribute("lang", lang);
-				
-				// Store in localStorage for future visits
-				localStorage.setItem("preferredLang", lang);
-				
-				// Mark that we've fetched IP info this session
-				sessionStorage.setItem("ipinfoFetched", "true");
-			})
-			.catch(err => console.error("IP info error:", err));
-		}
-	}, []);
-	
-	return null;
+    useEffect(async () => {
+        let html = document.documentElement;
+        let storedLang = localStorage.getItem('language');
+        
+        storedLang && html.setAttribute('lang', storedLang);
+        
+        let visited = sessionStorage.getItem('visited');
+        
+        if (!visited) {
+            try {
+                let res = await(await fetch('https://ipinfo.io/json')).json();
+                
+                let lang = {
+                    VN: 'vi', FR: 'fr', IT: 'it', KR: 'kr',
+                    JP: 'ja', DE: 'de', NL: 'nl', DK: 'dk',
+                    PT: 'pt', ES: 'es', RU: 'ru',
+                }[res.country] || 'en';
+                
+                html.setAttribute('lang', lang);
+                
+                localStorage.setItem('language', lang);
+                sessionStorage.setItem('visited', true);
+            }
+            catch (err) {
+                console.error('IP info error:', err)
+            }
+        }
+    }, [])
+    
+    return null;
 }
