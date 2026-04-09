2945122ba79bb2f1c82f06d4e19fcad358c05272
Namdev23 <nguyennamtiger@gmail.com>
2026-04-09 19:57:49 +0700

    09/04/26 (6)

---
+++ src/app/not-found.js
@@ -0,0 +1,9 @@
+// app/not-found.js
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 - Not Found</h1>
+      <p>Sorry, we couldn’t find that page.</p>
+    </div>
+  );
+}
