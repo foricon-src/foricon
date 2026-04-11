5dc9d75d2f9668e9119fd84d2a9a3c9c17a676f1
Namdev23 <nguyennamtiger@gmail.com>
2026-04-11 20:18:16 +0700

    11/04/26 (30.3)

--- src/utils/firebase-admin.js
+++ src/utils/firebase-admin.js
@@ -1,10 +1,15 @@
 import admin from 'firebase-admin';
-admin.initializeApp({
-    credential: admin.credential.cert({
-        project_id: process.env.FIREBASE_PROJECT_ID,
-        client_email: process.env.FIREBASE_CLIENT_EMAIL,
-        private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+
+export function getAdmin() {
+    admin.initializeApp({
+        credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        })
     })
-})
-const db = admin.firestore();
-export { admin, db };
+    return admin;
+}
+export function getDb() {
+    return getAdmin().firestore();
+}
