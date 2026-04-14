f72ce0e52b1f32d6d2f0e5bfdc9052310112da22
Namdev23 <nguyennamtiger@gmail.com>
2026-04-14 23:46:08 +0700

    14/04/26

--- src/components/process.js
+++ src/components/process.js
@@ -711,10 +711,16 @@ export default function Process() {
         }
 
         html.lang = language;
+
+        (async () => {
+            function get() {
+                qSelec(true, header_center, 'a');
+            }
+            while (!get()) await wait();
+            get().forEach(each => each.pathname == pathname ? activate(each) : inactivate(each));
+        })()
         
         while (user == null || user && !user.doc || !window.foriconPackageIsLoaded) await wait();
-
-        qSelec(true, header_center, 'a').forEach(each => each.pathname == pathname ? activate(each) : inactivate(each));
 
         let loading = elemById('loading');
         if (loading) {
