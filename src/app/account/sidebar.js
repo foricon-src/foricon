06e8a27c473c066665f3170f21ab9e05268ee511
Namdev23 <nguyennamtiger@gmail.com>
2026-04-16 00:55:19 +0700

    16/04/26 (3)

--- src/app/account/sidebar.js
+++ src/app/account/sidebar.js
@@ -10,7 +10,7 @@ export default function Sidebar() {
 
     function stateActive(path = '', isIcon) {
         path && (path = `/${path}`);
-        return pathname.startsWith(`/account${path}`)
+        return pathname.slice(8).startsWith(path)
             ? isIcon ? 'solid' : 'active'
             : isIcon ? 'outline' : '';
     }
