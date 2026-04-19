14e7f5e5f9b4e63a14463e4d2fa7af31af4afc9b
Namdev23 <nguyennamtiger@gmail.com>
2026-04-19 14:19:40 +0700

    19/04/26 (7.1)

--- src/components/sidebar-page-transition.js
+++ src/components/sidebar-page-transition.js
@@ -2,6 +2,7 @@
 
 import { motion } from 'framer-motion';
 import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 export function GoPage(page) {
     let router = useRouter();
@@ -13,9 +14,12 @@ export function GoPage(page) {
     })()
 }
 export function SidebarPageTransition({ name, children }) {
+    let pathname = usePathname();
+
     return (
         <motion.div
             name={name}
+            key={pathname}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: .2, ease: 'ease' }}
