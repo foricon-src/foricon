656404db54beac2fbed29e07485e25cfa2aa6b19
Namdev23 <nguyennamtiger@gmail.com>
2026-04-27 23:01:09 +0700

    27/04/26 (23.1)

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
-import withGoogleAdsense from 'next-google-adsense';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     /* config options here */
     reactCompiler: true,
     productionBrowserSourceMaps: true,
-    googleAdsense: { client: 'ca-pub-8532596750508498' },
 }
 
-export default withGoogleAdsense(nextConfig);
+export default nextConfig;
