13b8d588c48a951e0d679c6ad35b1cc17c916db4
Namdev23 <nguyennamtiger@gmail.com>
2026-04-30 23:08:50 +0700

    30/04/26 (21)

--- src/app/(header)/account/info/page.js
+++ src/app/(header)/account/info/page.js
@@ -6,114 +6,118 @@ import usePage from "Pag/(header)/account/use-page";
 import 'Pag/sidebar-page.css';
 import 'Pag/(header)/account/internal-global.css';
 import cssStyle from './page.module.css';
+import { useState } from "react";
 
 export default function Page() {
     let router = useRouter();
+    let [ u, setU ] = useState();
 
     usePage(() => {
-        elemById('avatar').style.backgroundImage = `url(${user.doc.avatar})`;
+        setU(user);
     })
 
     return (
         <div className={cssStyle.account_info}>
             <table>
-                <tr>
-                    <td>{
-                        GetLang({
-                            en: 'Avatar',
-                            vi: 'Ảnh đại diện',
-                            fr: 'Avatar',
-                            it: "L'Avatar",
-                            kr: '화신',
-                            ja: 'アバター',
-                            de: 'Avatar',
-                            nl: 'Avatar',
-                            dk: 'Avatar',
-                            pt: 'Avatar',
-                            es: 'Avatar',
-                            ru: 'Аватар',
-                        })
-                    }</td>
-                    <td>
-                    <div id='avatar' className='img circle square'></div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>{
-                        GetLang({
-                            en: 'Name',
-                            vi: 'Tên',
-                            fr: 'Nom',
-                            it: 'Nome',
-                            kr: '이름',
-                            ja: '名前',
-                            de: 'Name',
-                            nl: 'Naam',
-                            dk: 'Navn',
-                            pt: 'Nome',
-                            es: 'Nombre',
-                            ru: 'Имя',
-                        })
-                    }</td>
-                    <td id='main_account-info_username'>Username123</td>
-                </tr>
-                <tr>
-                    <td>{
-                        GetLang({
-                            en: 'Email',
-                            vi: 'Email',
-                            fr: 'Email',
-                            it: 'Email',
-                            kr: '이메일',
-                            ja: 'メール',
-                            de: 'Email',
-                            nl: 'Email',
-                            dk: 'Email',
-                            pt: 'Email',
-                            es: 'Correo electrónico',
-                            ru: 'Электронная почта',
-                        })
-                    }</td>
-                    <td id='main_account-info_email'>user@example.com</td>
-                </tr>
-                <tr>
-                    <td>{
-                        GetLang({
-                            en: 'User ID',
-                            vi: 'ID người dùng',
-                            fr: "ID de l'utilisateur",
-                            it: 'ID utente',
-                            kr: '사용자 ID',
-                            ja: 'ユーザーID',
-                            de: 'Benutzer-ID',
-                            nl: 'Gebruikers-ID',
-                            dk: 'Bruger-id',
-                            pt: 'ID do utilizador',
-                            es: 'ID de usuario',
-                            ru: 'ID пользователя',
-                        })
-                    }</td>
-                    <td id='main_account-info_uid'>123ABC</td>
-                </tr>
-                <tr>
-                    <td>{
-                        GetLang({
-                            en: 'Joining date',
-                            vi: 'Ngày tham gia',
-                            fr: "Date d'adhésion",
-                            it: 'Data di adesione',
-                            kr: '가입날짜',
-                            ja: '入社日',
-                            de: 'Eintrittsdatum',
-                            nl: 'Datum van toetreding',
-                            dk: 'Tilmeldingsdato',
-                            pt: 'Data de adesão',
-                            es: 'Fecha de ingreso',
-                            ru: 'Дата присоединения',
-                        })
-                    }</td>
-                    <td id='main_account-info_joining-date'>21 December, 2023</td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <td>{
+                            GetLang({
+                                en: 'Avatar',
+                                vi: 'Ảnh đại diện',
+                                fr: 'Avatar',
+                                it: "L'Avatar",
+                                kr: '화신',
+                                ja: 'アバター',
+                                de: 'Avatar',
+                                nl: 'Avatar',
+                                dk: 'Avatar',
+                                pt: 'Avatar',
+                                es: 'Avatar',
+                                ru: 'Аватар',
+                            })
+                        }</td>
+                        <td>
+                            <div className='img circle square' style={{ backgroundImage: `url(${u?.doc.avatar})` }}/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>{
+                            GetLang({
+                                en: 'Name',
+                                vi: 'Tên',
+                                fr: 'Nom',
+                                it: 'Nome',
+                                kr: '이름',
+                                ja: '名前',
+                                de: 'Name',
+                                nl: 'Naam',
+                                dk: 'Navn',
+                                pt: 'Nome',
+                                es: 'Nombre',
+                                ru: 'Имя',
+                            })
+                        }</td>
+                        <td>{u?.doc.name}</td>
+                    </tr>
+                    <tr>
+                        <td>{
+                            GetLang({
+                                en: 'Email',
+                                vi: 'Email',
+                                fr: 'Email',
+                                it: 'Email',
+                                kr: '이메일',
+                                ja: 'メール',
+                                de: 'Email',
+                                nl: 'Email',
+                                dk: 'Email',
+                                pt: 'Email',
+                                es: 'Correo electrónico',
+                                ru: 'Электронная почта',
+                            })
+                        }</td>
+                        <td>{u?.doc.email}{u?.emailVerified ? <f-icon icon='circle-check'/> : <span>Verify</span>}</td>
+                    </tr>
+                    <tr>
+                        <td>{
+                            GetLang({
+                                en: 'User ID',
+                                vi: 'ID người dùng',
+                                fr: "ID de l'utilisateur",
+                                it: 'ID utente',
+                                kr: '사용자 ID',
+                                ja: 'ユーザーID',
+                                de: 'Benutzer-ID',
+                                nl: 'Gebruikers-ID',
+                                dk: 'Bruger-id',
+                                pt: 'ID do utilizador',
+                                es: 'ID de usuario',
+                                ru: 'ID пользователя',
+                            })
+                        }</td>
+                        <td>{u?.uid}</td>
+                    </tr>
+                    <tr>
+                        <td>{
+                            GetLang({
+                                en: 'Joining date',
+                                vi: 'Ngày tham gia',
+                                fr: "Date d'adhésion",
+                                it: 'Data di adesione',
+                                kr: '가입날짜',
+                                ja: '入社日',
+                                de: 'Eintrittsdatum',
+                                nl: 'Datum van toetreding',
+                                dk: 'Tilmeldingsdato',
+                                pt: 'Data de adesão',
+                                es: 'Fecha de ingreso',
+                                ru: 'Дата присоединения',
+                            })
+                        }</td>
+                        <td id='main_account-info_joining-date'>21 December, 2023</td>
+                    </tr>
+                </tbody>
             </table>
             <ul className='btn-list vertical large'>
                 <li onClick={() => go(router, 'account/info/edit')}>{
