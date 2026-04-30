de280296a3e0a9cd6d443c6dcd11cf9daabb4c8e
Namdev23 <nguyennamtiger@gmail.com>
2026-04-30 13:23:24 +0700

    30/04/26 (4)

---
+++ src/app/(header-footer)/search/initial.js
@@ -0,0 +1,39 @@
+export default function Initial(search) {
+    if (!searchParams) {
+        return {
+            search: '',
+            family: 'all',
+            style: 'all',
+            version: 'b2',
+            categories: [],
+        }
+    }
+    
+    let map = Object.fromEntries(
+        search.slice(1).split('&').map(p => [ p.slice(0, 2), p.slice(2) ])
+    )
+    
+    function verify(param) {
+        if (param == 'k') return map['k='] || '';
+        if (param == 'f') return {
+            regular: 'regular',
+            duotone: 'duotone',
+        }[map['f=']] || 'all';
+        if (param == 's') return {
+            solid: 'solid',
+            outline: 'outline'
+        }[map['s=']] || 'all';
+        if (param == 'v') return {
+            b1: 'b1',
+        }[map['v=']] || 'b2';
+        if (param == 'c') return (map['c=']?.split(';') || []).filter(category => webData.categories[category]);
+    }
+    
+    return {
+        search: verify('k'),
+        family: verify('f'),
+        style: verify('s'),
+        version: verify('v'),
+        categories: verify('c'),
+    }
+}
--- src/app/(header-footer)/search/page.js
+++ src/app/(header-footer)/search/page.js
@@ -2,48 +2,10 @@ import Search from "./pageClient";
 import FilterIcon from "./toIconMap";
 import { get, ref } from "firebase/database";
 import { db } from "Com/firebase";
+import Initial from "./initial";
 
 export async function Page({ searchParams }) {
-    let initial = (() => {
-        if (!searchParams) {
-            return {
-                search: '',
-                family: 'all',
-                style: 'all',
-                version: 'b2',
-                categories: [],
-            }
-        }
-        
-        let params = searchParams.slice(1);
-        let map = Object.fromEntries(
-            params.split('&').map(p => [ p.slice(0, 2), p.slice(2) ])
-        )
-        
-        function verify(param) {
-            if (param == 'k') return map['k='] || '';
-            if (param == 'f') return {
-                regular: 'regular',
-                duotone: 'duotone',
-            }[map['f=']] || 'all';
-            if (param == 's') return {
-                solid: 'solid',
-                outline: 'outline'
-            }[map['s=']] || 'all';
-            if (param == 'v') return {
-                b1: 'b1',
-            }[map['v=']] || 'b2';
-            if (param == 'c') return (map['c=']?.split(';') || []).filter(category => webData.categories[category]);
-        }
-        
-        return {
-            search: verify('k'),
-            family: verify('f'),
-            style: verify('s'),
-            version: verify('v'),
-            categories: verify('c'),
-        }
-    })()
+    let initial = Initial(searchParams);
 
     let snap = await get(ref(db, `icons${initial.version == 'b2' ? 'B2' : ''}/`))
     let icons = snap.val();
@@ -61,7 +23,7 @@ export async function Page({ searchParams }) {
             <div id='adsense-content'>
                 <h1>{filtered.length} icons</h1>
                 <ul>{
-                    icons.slice(0,100).map(i =>
+                    filtered.slice(0,100).map(i =>
                         <li key={i.name}>{i.name}</li>
                     )
                 }</ul>
