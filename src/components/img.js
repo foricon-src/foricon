dac4422d5b18a098c4572c6aadb4484251780dd7
Namdev23 <nguyennamtiger@gmail.com>
2026-05-03 19:58:27 +0700

    03/05/26 (5)

--- src/components/img.js
+++ src/components/img.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Img({ src, alt }) {
-    return <Image src={src} alt={alt} unoptimized/>;
+    return <Image src={src} alt={alt} height={0} width={0}/>;
 }
