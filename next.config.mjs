7829a460dffd6fe48847ac0df2a312a3ba45b711
Namdev23 <nguyennamtiger@gmail.com>
2026-05-11 20:47:20 +0700

    11/05/26 (10)

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
     /* config options here */
     reactCompiler: true,
     productionBrowserSourceMaps: true,
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'strict-origin-when-cross-origin',
+                    },
+                ],
+            },
+        ]
+    }
 }
 
 export default nextConfig;
