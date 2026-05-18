51fb07955be5d514f9d4e1ca67ae80919946f5c5
Namdev23 <nguyennamtiger@gmail.com>
2026-05-18 22:42:54 +0700

    18/05/26 (21)

--- src/components/icons.js
+++ src/components/icons.js
@@ -9,31 +9,34 @@ export function IconProvider({ children }) {
     let [ icons, setIcons ] = useState(null);
 
     useEffect(() => {(async () => {
-        let value = { b1: [], b2: [] };
-        
-        let b1 = (await get(ref(db, 'icons/'))).val();
-        for (let name in b1) {
-            let icon = b1[name];
-            value.b1.push({
-                name,
-                categories: icon.categories.map(cate => cate.replace('bussiness', 'business')),
-                styles: icon.styles,
-                glyphs: icon.glyphs,
-                unicodes: icon.unicodes,
-            })
+        try {
+            let value = { b1: [], b2: [] };
+            
+            let b1 = (await get(ref(db, 'icons/'))).val();
+            for (let name in b1) {
+                let icon = b1[name];
+                value.b1.push({
+                    name,
+                    categories: icon.categories.map(cate => cate.replace('bussiness', 'business')),
+                    styles: icon.styles,
+                    glyphs: icon.glyphs,
+                    unicodes: icon.unicodes,
+                })
+            }
+            let b2 = (await get(ref(db, 'iconsB2/'))).val();
+            for (let name in b2) {
+                let icon = b2[name];
+                value.b2.push({
+                    name,
+                    categories: icon.categories,
+                    styles: icon.styles,
+                    glyphs: icon.glyphs,
+                    unicodes: icon.unicodes,
+                })
+            }
+            setIcons(value);
         }
-        let b2 = (await get(ref(db, 'iconsB2/'))).val();
-        for (let name in b2) {
-            let icon = b2[name];
-            value.b2.push({
-                name,
-                categories: icon.categories,
-                styles: icon.styles,
-                glyphs: icon.glyphs,
-                unicodes: icon.unicodes,
-            })
-        }
-        setIcons(value);
+        catch (e) { console.error(e) }
     })}, [])
 
     return <IconContext.Provider value={icons}>{
