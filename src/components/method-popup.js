4e1cdd43d6a0a6eec73f631118b94dfd5763071c
Namdev23 <nguyennamtiger@gmail.com>
2026-05-18 21:40:19 +0700

    18/05/26 (9)

--- src/components/method-popup.js
+++ src/components/method-popup.js
@@ -4,17 +4,21 @@ import { useRouter } from 'next/navigation';
 import { signInWithPopup } from 'firebase/auth';
 import { auth } from './firebase';
 import recordLogin from './record-login';
+import { useEffect } from 'react';
 
-export default async function MethodPopup(provider, des) {
+export default function MethodPopup(provider, des) {
     let router = useRouter();
-    let result = await signInWithPopup(auth, provider);
-    let token = await result.user.getIdToken();
 
-    await fetch('/api/user/create', {
-        method: 'POST',
-        headers: { Authorization: `Bearer ${token}` },
-    })
-    await recordLogin(token);
+    useEffect(() => {(async () => {
+        let result = await signInWithPopup(auth, provider);
+        let token = await result.user.getIdToken();
+        
+        await fetch('/api/user/create', {
+            method: 'POST',
+            headers: { Authorization: `Bearer ${token}` },
+        })
+        await recordLogin(token);
 
-    go(router, des);
+        go(router, des);
+    })()}, [])
 }
