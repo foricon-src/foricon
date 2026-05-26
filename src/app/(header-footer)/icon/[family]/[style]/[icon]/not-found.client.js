90f5d894cb2b2b31095754d7407de830a0e4d9fd
Namdev23 <nguyennamtiger@gmail.com>
2026-05-26 22:35:59 +0700

    26/05/26 (7)

---
+++ src/app/(header-footer)/icon/[family]/[style]/[icon]/not-found.client.js
@@ -0,0 +1,8 @@
+export function NotFound({ family, styles, icon }) {
+    return (
+        <div>
+            <f-icon icon='face-frown'/>
+            <h1>Sorry, we can't find that</h1>
+        </div>
+    )
+}
--- src/app/(header-footer)/icon/[family]/[style]/[icon]/page.client.js
+++ src/app/(header-footer)/icon/[family]/[style]/[icon]/page.client.js
@@ -1,33 +1,41 @@
 'use client';
 
+import { useContext } from 'react';
+import { useRouter } from 'next/navigation';
+import { IconContext } from 'Com/icons';
 import Code from 'Com/code';
+import { NotFound } from './not-found.client';
 import cssStyle from './page.module.css';
 
 export default function PageClient({ params }) {
     let { family, style, icon } = JSON.parse(params.value);
     let styleName = [ family == 'duotone' && family, style ].filter(Boolean).join('/');
 
-    return (
+    let router = useRouter();
+    let icons = useContext(IconContext);
+    let currentIcon = icons.find(i => i.name == icon);
+
+    return currentIcon ? (
         <div className={cssStyle.main}>
-            <h1>{icon}</h1>
+            <h3>{icon}</h3>
             <div>
                 <f-icon icon={icon} i-s={styleName}/>
             </div>
             <ul className='btn-list'>
-                <li>
+                {currentIcon.styles.includes('solid') && <li onClick={() => go(router, `icon/regular/solid/${icon}`)}>
                     <f-icon icon={icon}/>
-                </li>
-                <li>
+                </li>}
+                {currentIcon.styles.includes('outline') && <li onClick={() => go(router, `icon/regular/outline/${icon}`)}>
                     <f-icon icon={icon} i-s='outline'/>
-                </li>
-                <li>
+                </li>}
+                {currentIcon.styles.includes('duotone-solid') && <li onClick={() => go(router, `icon/duotone/solid/${icon}`)}>
                     <f-icon icon={icon} i-s='duotone/solid'/>
-                </li>
-                <li>
+                </li>}
+                {currentIcon.styles.includes('duotone-outline') && <li onClick={() => go(router, `icon/duotone/outline/${icon}`)}>
                     <f-icon icon={icon} i-s='duotone/outline'/>
-                </li>
+                </li>}
             </ul>
-            <Code>{`<f-icon icon=${icon} i-s='${styleName}'`}</Code>
+            <Code>{`<f-icon icon='${icon}' i-s='${styleName}'></f-icon>`}</Code>
         </div>
-    )
+    ) : <NotFound family={family} style={style} icon={icon}/>;
 }
