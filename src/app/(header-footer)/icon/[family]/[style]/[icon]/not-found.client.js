73478452967b019c4af88d598e2ad71699062df2
Namdev23 <nguyennamtiger@gmail.com>
2026-05-27 01:02:28 +0700

    27/05/26 (8)

--- src/app/(header-footer)/icon/[family]/[style]/[icon]/not-found.client.js
+++ src/app/(header-footer)/icon/[family]/[style]/[icon]/not-found.client.js
@@ -2,9 +2,9 @@ import cssStyle from './not-found.module.css';
 
 export function NotFound({ family, style, icon }) {
     return (
-        <div className={cssStyle.main}>
-            <f-icon icon='face-frown'/>
-            <h1>Sorry, we could not find that</h1>
+        <div className={`center-middle ${cssStyle.main}`}>
+            <f-icon icon='face-frown' className='auto-line-height'/>
+            <h3>Sorry, we could not find that</h3>
         </div>
     )
 }
