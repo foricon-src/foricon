b27ebd79ed996391ab3bcc397f4e9a9f4637cea4
Namdev23 <nguyennamtiger@gmail.com>
2026-05-26 22:39:34 +0700

    26/05/26 (8)

--- src/app/(header-footer)/icon/[family]/[style]/[icon]/not-found.client.js
+++ src/app/(header-footer)/icon/[family]/[style]/[icon]/not-found.client.js
@@ -1,8 +1,8 @@
-export function NotFound({ family, styles, icon }) {
+export function NotFound({ family, style, icon }) {
     return (
         <div>
             <f-icon icon='face-frown'/>
-            <h1>Sorry, we can't find that</h1>
+            <h1>Sorry, we could not find that</h1>
         </div>
     )
 }
