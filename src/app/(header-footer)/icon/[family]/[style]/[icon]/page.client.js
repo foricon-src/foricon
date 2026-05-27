a8c31e20d0866ce07106ef2ecaf745c487eb7525
Namdev23 <nguyennamtiger@gmail.com>
2026-05-27 19:33:34 +0700

    27/05/26 (24)

--- src/app/(header-footer)/icon/[family]/[style]/[icon]/page.client.js
+++ src/app/(header-footer)/icon/[family]/[style]/[icon]/page.client.js
@@ -59,13 +59,13 @@ export default function PageClient({ params }) {
                     <div>
                         <h6>Unicodes</h6>
                         <ul className='btn-list'>{
-                            currentIcon.unicodes[styleName].split('|').map(i => <li key={i}>{i}</li>)
+                            currentIcon.unicodes[styleName.replace('/', '-')].split('|').map(i => <li key={i}>{i}</li>)
                         }</ul>
                     </div>
                     <div>
                         <h6>Glyphs</h6>
                         <ul className='btn-list'>{
-                            currentIcon.glyphs[styleName].split('').map(i => <li key={i}>{i}</li>)
+                            currentIcon.glyphs[styleName.replace('/', '-')].split('').map(i => <li key={i}>{i}</li>)
                         }</ul>
                     </div>
                     <div>
