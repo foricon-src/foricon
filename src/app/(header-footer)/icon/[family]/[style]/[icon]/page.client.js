ac78c7dffab9db7e4d88db5b0cf9ae9c7c9ffbe8
Namdev23 <nguyennamtiger@gmail.com>
2026-05-27 18:52:30 +0700

    27/05/26 (17)

--- src/app/(header-footer)/icon/[family]/[style]/[icon]/page.client.js
+++ src/app/(header-footer)/icon/[family]/[style]/[icon]/page.client.js
@@ -17,7 +17,17 @@ export default function PageClient({ params }) {
     
     return currentIcon && currentIcon.styles.includes(styleName) ? (
         <div className={cssStyle.main}>
-            <h3>{icon}</h3>
+            <div className={cssStyle.top}>
+                <h3>{icon}</h3>
+                <ul className='btn-list'>{
+                    currentIcon.categories.map(category => {
+                        let { icon, ...texts } = webData.categories[category];
+                        return <li key={category} dangerouslySetInnerHTML={{
+                            __html: `${icon}<span>${texts[lang]}</span>`,
+                        }} onClick={() => go(router, `search?c=${category}`)}/>
+                    })
+                }</ul>
+            </div>
             <div>
                 <div className={`center-middle ${cssStyle.iconView}`}>
                     <f-icon icon={icon} i-s={styleName} className='auto-line-height'/>
@@ -42,14 +52,24 @@ export default function PageClient({ params }) {
                         }
                     </ul>
                     <Code>{`<f-icon icon='${icon}'${styleName == 'solid' ? '' : ` i-s='${styleName}'`}></f-icon>`}</Code>
-                    <ul className='btn-list'>{
-                        [ 'SVG', 'PNG', 'WebP' ].map(i =>
-                            <li key={i}>
-                                <f-icon icon='arrow-down-to-bracket' i-s='outline'/>
-                                <span>{i}</span>
-                            </li>
-                        )
-                    }</ul>
+                    <div>
+                        <h6>Unicodes</h6>
+                        <ul className='btn-list'>{
+                            currentIcon.unicodes.split('|').map(i => <li key={i}>{i}</li>)
+                        }</ul>
+                    </div>
+                    <div>
+                        <h6>Glyphs</h6>
+                        <ul className='btn-list'>{
+                            currentIcon.glyphs.split('').map(i => <li key={i}>{i}</li>)
+                        }</ul>
+                    </div>
+                    <div>
+                        <h6>Download</h6>
+                        <ul className='btn-list'>{
+                            [ 'SVG', 'PNG', 'WebP' ].map(i => <li key={i}>{i}</li>)
+                        }</ul>
+                    </div>
                 </div>
             </div>
         </div>
