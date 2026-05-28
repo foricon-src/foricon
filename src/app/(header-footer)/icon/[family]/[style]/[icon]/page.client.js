177a3938ab5a804f3d9a1d59d7997a8a47913e05
Namdev23 <nguyennamtiger@gmail.com>
2026-05-28 11:48:35 +0700

    28/05/26 (10)

--- src/app/(header-footer)/icon/[family]/[style]/[icon]/page.client.js
+++ src/app/(header-footer)/icon/[family]/[style]/[icon]/page.client.js
@@ -17,42 +17,103 @@ export default function PageClient({ params }) {
     let icons = useContext(IconContext);
     let lang = useContext(LanguageContext);
     let currentIcon = useMemo(() => icons?.b2.find(i => i.name == icon), [ icons, icon ]);
+
+    let copiedToClipboard = {
+        en: 'Copied to clipboard',
+        vi: 'Đã sao chép vào bộ nhớ tạm',
+        fr: 'Copié dans le presse-papiers',
+        it: 'Copiato negli appunti',
+        kr: '클립보드에 복사됨',
+        ja: 'クリップボードにコピーされました',
+        de: 'In die Zwischenablage kopiert',
+        nl: 'Gekopieerd naar het klembord',
+        dk: 'Kopieret til udklipsholderen',
+        pt: 'Copiado para a área de transferência',
+        es: 'Copiado al portapapeles',
+        ru: 'Скопировано в буфер обмена',
+    }[lang]
     
     return !!currentIcon && currentIcon.styles.includes(styleName)
         ? <div className={cssStyle.wrapper}>
             <div className={cssStyle.top}>
                 <h3>{icon}</h3>
                 <ul className='btn-list'>
-                    <li>{
-                        true ? {
-                            en: 'Save',
-                            vi: 'Lưu',
-                            fr: 'Sauvegarder',
-                            it: 'Salva',
-                            kr: '구하다',
-                            ja: '保存',
-                            de: 'Speichern',
-                            nl: 'Redden',
-                            dk: 'Spare',
-                            pt: 'Guardar',
-                            es: 'Ahorrar',
-                            ru: 'Сохранять',
-                        }[lang] :
-                        {
-                            en: 'Saved',
-                            vi: 'Đã lưu',
-                            fr: 'Sauvé',
-                            it: 'Salvato',
-                            kr: '저장됨',
-                            ja: '保存済み',
-                            de: 'Gespeichert',
-                            nl: 'Opgeslagen',
-                            dk: 'Gemt',
-                            pt: 'Salvo',
-                            es: 'Guardado',
-                            ru: 'Сохранено',
-                        }[lang]
-                    }</li>
+                    <li onClick={() => {
+                        navigator.clipboard.writeText(icon);
+                        notify('success', copiedToClipboard);
+                    }}>
+                        <f-icon icon='clone'/>
+                        <span>{
+                            {
+                                en: 'Copy icon name',
+                                vi: 'Sao chép tên biểu tượng',
+                                fr: "Copier le nom de l'icône",
+                                it: "Copia il nome dell'icona",
+                                kr: '아이콘 이름 복사',
+                                ja: 'アイコン名をコピー',
+                                de: 'Symbolnamen kopieren',
+                                nl: 'Kopieer pictogramnaam',
+                                dk: 'Kopiér ikonnavn',
+                                pt: 'Copiar nome do ícone',
+                                es: 'Copiar el nombre del icono',
+                                ru: 'Скопировать название значка',
+                            }[lang]
+                        }</span>
+                    </li>
+                    <li onClick={() => {
+                        navigator.clipboard.writeText(location.href);
+                        notify('success', copiedToClipboard);
+                    }}>
+                        <f-icon icon='link' i-s='outline'/>
+                        <span>{
+                            {
+                                en: 'Copy link',
+                                vi: 'Sao chép liên kết',
+                                fr: 'Copier le lien',
+                                it: 'Copia il link',
+                                kr: '링크 복사',
+                                ja: 'リンクをコピー',
+                                de: 'Link kopieren',
+                                nl: 'Kopieer link',
+                                dk: 'Kopiér link',
+                                pt: 'Copiar link',
+                                es: 'Copiar enlace',
+                                ru: 'Скопировать ссылку',
+                            }[lang]
+                        }</span>
+                    </li>
+                    <li className='chip top'>
+                        <span>{
+                            true ? {
+                                en: 'Save',
+                                vi: 'Lưu',
+                                fr: 'Sauvegarder',
+                                it: 'Salva',
+                                kr: '구하다',
+                                ja: '保存',
+                                de: 'Speichern',
+                                nl: 'Redden',
+                                dk: 'Spare',
+                                pt: 'Guardar',
+                                es: 'Ahorrar',
+                                ru: 'Сохранять',
+                            }[lang] :
+                            {
+                                en: 'Saved',
+                                vi: 'Đã lưu',
+                                fr: 'Sauvé',
+                                it: 'Salvato',
+                                kr: '저장됨',
+                                ja: '保存済み',
+                                de: 'Gespeichert',
+                                nl: 'Opgeslagen',
+                                dk: 'Gemt',
+                                pt: 'Salvo',
+                                es: 'Guardado',
+                                ru: 'Сохранено',
+                            }[lang]
+                        }</span>
+                    </li>
                 </ul>
                 <ul className='btn-list'>{
                     currentIcon.categories.map(category => {
@@ -91,49 +152,53 @@ export default function PageClient({ params }) {
                 <div>
                     <Code>{`<f-icon icon='${icon}'${styleName == 'solid' ? '' : ` i-s='${styleName}'`}></f-icon>`}</Code>
                     <div>
-                        <h6>Unicodes</h6>
+                        <h6>{
+                            {
+                                en: 'Unicodes',
+                                vi: 'Unicode',
+                                fr: 'Unicodes',
+                                it: 'Unicode',
+                                kr: '유니코드',
+                                ja: 'ユニコード',
+                                de: 'Unicodes',
+                                nl: 'Unicodes',
+                                dk: 'Unicodes',
+                                pt: 'Unicodes',
+                                es: 'Unicodes',
+                                ru: 'Юникоды',
+                            }[lang]
+                        }</h6>
                         <ul className='btn-list'>{
                             currentIcon.unicodes[styleName.replace('/', '-')].split('|').map(i =>
                                 <li key={i} onClick={() => {
                                     navigator.clipboard.writeText(i);
-                                    notify('success', {
-                                        en: 'Copied to clipboard',
-                                        vi: 'Đã sao chép vào bộ nhớ tạm',
-                                        fr: 'Copié dans le presse-papiers',
-                                        it: 'Copiato negli appunti',
-                                        kr: '클립보드에 복사됨',
-                                        ja: 'クリップボードにコピーされました',
-                                        de: 'In die Zwischenablage kopiert',
-                                        nl: 'Gekopieerd naar het klembord',
-                                        dk: 'Kopieret til udklipsholderen',
-                                        pt: 'Copiado para a área de transferência',
-                                        es: 'Copiado al portapapeles',
-                                        ru: 'Скопировано в буфер обмена',
-                                    }[lang]);
+                                    notify('success', copiedToClipboard);
                                 }}>{i}</li>
                             )
                         }</ul>
                     </div>
                     <div>
-                        <h6>Glyphs</h6>
+                        <h6>{
+                            {
+                                en: 'Glyphs',
+                                vi: 'Glyph',
+                                fr: 'Glyphes',
+                                it: 'Glifi',
+                                kr: '글리프',
+                                ja: 'グリフ',
+                                de: 'Glyphen',
+                                nl: 'Tekens',
+                                dk: 'Glyffer',
+                                pt: 'Glifos',
+                                es: 'Glifos',
+                                ru: 'Глифы',
+                            }[lang]
+                        }</h6>
                         <ul className='btn-list'>{
                             currentIcon.glyphs[styleName.replace('/', '-')].split('').map(i =>
                                 <li key={i} onClick={() => {
                                     navigator.clipboard.writeText(i);
-                                    notify('success', {
-                                        en: 'Copied to clipboard',
-                                        vi: 'Đã sao chép vào bộ nhớ tạm',
-                                        fr: 'Copié dans le presse-papiers',
-                                        it: 'Copiato negli appunti',
-                                        kr: '클립보드에 복사됨',
-                                        ja: 'クリップボードにコピーされました',
-                                        de: 'In die Zwischenablage kopiert',
-                                        nl: 'Gekopieerd naar het klembord',
-                                        dk: 'Kopieret til udklipsholderen',
-                                        pt: 'Copiado para a área de transferência',
-                                        es: 'Copiado al portapapeles',
-                                        ru: 'Скопировано в буфер обмена',
-                                    }[lang]);
+                                    notify('success', copiedToClipboard);
                                 }}><f-icon>{i}</f-icon></li>
                             )
                         }</ul>
