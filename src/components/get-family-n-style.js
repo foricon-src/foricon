ae37bafe79823d88a36fdfe3414f456380a7b7ba
Namdev23 <nguyennamtiger@gmail.com>
2026-05-30 15:21:35 +0700

    30/05/26 (10)

--- src/components/get-family-n-style.js
+++ src/components/get-family-n-style.js
@@ -1,5 +1,5 @@
 export default function GetFamilyAndStyle(styleName) {
-    let [ a, b ] = selectedIcon.style.split('/');
+    let [ a, b ] = styleName.split('/');
     let f = b ? a : 'regular';
     let s = b || a;
     return { f, s };
