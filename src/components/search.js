f4fc9053a18f7d3968b478d67a00cda1f29695ac
Namdev23 <nguyennamtiger@gmail.com>
2026-06-04 20:28:42 +0700

    04/06/26 (4)

--- src/components/search.js
+++ src/components/search.js
@@ -3,28 +3,16 @@
 import { useRef, useState } from 'react';
 
 export default function Search({ placeholder, onFocus, onBlur, onInput, ...rest }) {
-    let [ isFocused, setIsFocused ] = useState(false);
     let [ value, setValue ] = useState('');
     let inputRef = useRef(null);
-    let icon = value.length > 0 && isFocused ? 'xmark' : 'magnifying-glass';
+    let icon = value.length > 0 ? 'xmark' : 'magnifying-glass';
 
     return <label>
         <f-icon icon={icon} i-s='outline' onClick={() => icon == 'xmark' && (inputRef.current.value = '')}/>
         <input
             placeholder={placeholder}
             ref={inputRef}
-            onFocus={e => {
-                console.log('Focus');
-                setIsFocused(true);
-                onFocus?.(e);
-            }}
-            onBlur={e => {
-                console.log('Blur');
-                setIsFocused(false);
-                onBlur?.(e);
-            }}
             onInput={e => {
-                console.log('Input');
                 setValue(e.currentTarget.value);
                 onInput?.(e);
             }}
