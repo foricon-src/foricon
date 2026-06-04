84682833b5b61cd069c7442318cdf864d1447b60
Namdev23 <nguyennamtiger@gmail.com>
2026-06-04 21:41:46 +0700

    04/06/26 (13)

--- src/components/search.js
+++ src/components/search.js
@@ -1,34 +1,25 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import Join from './join';
-import './utils';
 
 export default function Search({ placeholder, value = '', onFocus, onBlur, onInput, ...rest }) {
     let [ val, setVal ] = useState(value);
-    let inputRef = useRef();
-    let iconRef = useRef();
+    let inputRef = useRef(null);
     let icon = val.length > 0 ? 'xmark' : 'magnifying-glass';
 
-    useEffect(() => {
-        addEvLis(iconRef.current, 'click', () => {
+    return <label className='search'>
+        <f-icon icon={icon} i-s='outline' onClick={() => {
             if (icon == 'xmark') return;
-            console.log(current.value)
             let { current } = inputRef;
             current.value = '';
-            console.log(current.value)
             current.dispatchEvent(new Event('input'));
-        })
-    }, [])
-
-    return <label className='search'>
-        <f-icon icon={icon} i-s='outline' ref={iconRef}/>
+        }}/>
         <input
             placeholder={placeholder}
             ref={inputRef}
             value={value}
             onInput={e => {
-                console.log(e.currentTarget.value)
                 setVal(e.currentTarget.value);
                 onInput?.(e);
             }}
