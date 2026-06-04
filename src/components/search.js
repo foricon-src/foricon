b60ee3993d06e8c2b755c8c04b79439e77f09026
Namdev23 <nguyennamtiger@gmail.com>
2026-06-04 20:48:37 +0700

    04/06/26 (8)

--- src/components/search.js
+++ src/components/search.js
@@ -3,23 +3,25 @@
 import { useRef, useState } from 'react';
 import Join from './join';
 
-export default function Search({ placeholder, className, value = '', onFocus, onBlur, onInput, ...rest }) {
+export default function Search({ placeholder, value = '', onFocus, onBlur, onInput, ...rest }) {
     let [ val, setVal ] = useState(value);
     let inputRef = useRef(null);
     let icon = val.length > 0 ? 'xmark' : 'magnifying-glass';
 
-    return <label>
+    return <label className='search'>
         <f-icon icon={icon} i-s='outline' onClick={() => {
             if (icon == 'xmark') return;
             let { current } = inputRef;
+            console.log(current.value)
             current.value = '';
             current.dispatchEvent(new Event('input'));
         }}/>
         <input
             placeholder={placeholder}
             ref={inputRef}
-            className={Join(' ', 'search', className)}
+            value={value}
             onInput={e => {
+                console.log(e.currentTarget.value)
                 setVal(e.currentTarget.value);
                 onInput?.(e);
             }}
