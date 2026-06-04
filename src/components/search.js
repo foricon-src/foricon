707d4f1ce93275590704b7449d81b2c255ba5308
Namdev23 <nguyennamtiger@gmail.com>
2026-06-04 20:35:09 +0700

    04/06/26 (6)

--- src/components/search.js
+++ src/components/search.js
@@ -1,8 +1,9 @@
 'use client';
 
 import { useRef, useState } from 'react';
+import Join from './join';
 
-export default function Search({ placeholder, value = '', onFocus, onBlur, onInput, ...rest }) {
+export default function Search({ placeholder, className, value = '', onFocus, onBlur, onInput, ...rest }) {
     let [ val, setVal ] = useState(value);
     let inputRef = useRef(null);
     let icon = val.length > 0 ? 'xmark' : 'magnifying-glass';
@@ -12,6 +13,7 @@ export default function Search({ placeholder, value = '', onFocus, onBlur, onInp
         <input
             placeholder={placeholder}
             ref={inputRef}
+            className={Join(' ', 'search', className)}
             onInput={e => {
                 setVal(e.currentTarget.value);
                 onInput?.(e);
