c462535b953840aeca83880d44207eda5b28d7fb
Namdev23 <nguyennamtiger@gmail.com>
2026-08-03 14:16:54 +0700

    03/08/26

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import Script from 'next/script';
+import { headers } from 'next/headers';
 import { GoogleAdSense } from 'next-google-adsense';
 import Loading from 'Com/loading';
 import Process from 'Com/process';
@@ -13,16 +14,18 @@ export const metadata = {
     icons: { icon: '/foricon-f-fav.png' },
     alternates: AlternativeLanguages(''),
 }
-export default function RootLayout({ children, params }) {
+export default function RootLayout({ children }) {
+    let lang = headers().get('f-lang');
+
     return (
-        <html lang={params.lang}>
+        <html lang={lang}>
             <head>
                 <link href='//fonts.googleapis.com' rel='preconnect'/>
                 <link crossOrigin='' href='//fonts.gstatic.com' rel='preconnect'/>
                 <link href='//fonts.googleapis.com/css2?family=Afacad:ital,wght@0,400..700;1,400..700&family=Fira+Code:wght@300..700&family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=Kode+Mono:wght@400..700&family=Nova+Mono&family=Sono:wght,MONO@200..800,1&family=Xanh+Mono:ital@0;1' rel='stylesheet'/>
             </head>
             <body className='cons'>
-                <GoogleAdSense client='pub-8532596750508498'/>
+                {/* <GoogleAdSense client='pub-8532596750508498'/> */}
                 <Providers>
                     <Script src='/api/import/ud4lP1mhq4XvynG7qUlcsAxi0Q02' strategy='afterInteractive'/>
                     <Loading/>
